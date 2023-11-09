d4ab27e7a824a95169c35514de2684a6db575a46
Riccardo Brasca <riccardo.brasca@gmail.com>
2023-11-09 13:57:40 +0100

    update this

--- TypeTheory.lean
+++ TypeTheory.lean
@@ -3,6 +3,7 @@ import TypeTheory.L2.ex
 import TypeTheory.L3.ex
 import TypeTheory.L4.ex
 import TypeTheory.L5.ex
+import TypeTheory.L6.ex
 import TypeTheory.Exercises.ex1
 import TypeTheory.Exercises.ex2
 import TypeTheory.Exercises.Solutions.sol1
